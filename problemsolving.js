664154694125e6ecbe7184b1e41bc458aabd7108
SHRIRAM-DB <shriramdb06@gmail.com>
2024-09-14 21:51:23 +0530

    Update problemsolving.js

--- problemsolving.js
+++ problemsolving.js
@@ -5,5 +5,24 @@ if(num%2==0){
 else{
     console.log("not even");
 }
+let age=10000;
+if(age<5){
+    console.log("free");
+}
+else if(age>=5 && age<=12){
+    console.log("10$")
+}
+
+else if(age>=13 && age<=64){
+    console.log("20$");
+}
+
+else if(age>=65){
+    console.log("15$")
+}
+
+else{
+    console.log("ticket is not free")
+}
 
 
